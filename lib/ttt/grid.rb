5bb5f0a24b36dd5ada53c56b3bee9383d25860a1
Samuel Molinari <samuel@molinari.me>
2014-11-06 09:54:27 +0000

    set sizing constants

--- lib/ttt/grid.rb
+++ lib/ttt/grid.rb
@@ -2,4 +2,7 @@ require_relative '../ttt.rb'
 
 class ::TTT::Grid
 
+  SIZE  = 3
+  CELLS = SIZE**2
+
 end
--- test/spec/ttt/grid_spec.rb
+++ test/spec/ttt/grid_spec.rb
@@ -2,6 +2,12 @@ require 'ttt/grid'
 
 describe ::TTT::Grid do
 
-  
+  it 'has a size of 3 by 3' do
+    expect(described_class::SIZE).to be 3
+  end
+
+  it 'has 9 cells' do
+    expect(described_class::CELLS).to be 9
+  end
 
 end
