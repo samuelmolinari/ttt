25c443bb8eeebc7217a0ae14a762d7ba423ddf76
Samuel Molinari <samuel@molinari.me>
2014-11-06 10:00:44 +0000

    add store to grid

--- lib/ttt/grid.rb
+++ lib/ttt/grid.rb
@@ -2,7 +2,17 @@ require_relative '../ttt.rb'
 
 class ::TTT::Grid
 
+  attr_reader :store
+
   SIZE  = 3
   CELLS = SIZE**2
+
+  def initialize
+    @store = Array.new(CELLS)
+  end
+
+  def size
+    @store.size
+  end
 
 end
--- test/spec/ttt/grid_spec.rb
+++ test/spec/ttt/grid_spec.rb
@@ -10,4 +10,14 @@ describe ::TTT::Grid do
     expect(described_class::CELLS).to be 9
   end
 
+  subject(:grid) { described_class.new }
+
+  it { is_expected.to have_attributes(:store => Array.new(described_class::CELLS)) }
+
+  describe '#size' do
+    it 'returns the number of cells' do
+      expect(grid.size).to be described_class::CELLS
+    end
+  end
+
 end
